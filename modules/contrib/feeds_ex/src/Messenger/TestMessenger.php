888a94027c0533c8114e8de846bd155a98e024e7
Arthur Wilson <a.wilson@ed.ac.uk>
2018-09-04 14:37:28 +0100

    added the feeds extensible parsers module

---
+++ modules/contrib/feeds_ex/src/Messenger/TestMessenger.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\feeds_ex\Messenger;
+
+/**
+ * Stores messages without calling drupal_set_mesage().
+ */
+class TestMessenger implements MessengerInterface {
+
+  /**
+   * The messages that have been set.
+   *
+   * @var array
+   */
+  protected $messages = [];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setMessage($message = NULL, $type = 'status', $repeat = TRUE) {
+    $this->messages[] = [
+      'message' => $message,
+      'type' => $type,
+      'repeat' => $repeat,
+    ];
+  }
+
+  /**
+   * Returns the messages.
+   *
+   * This is used to inspect messages that have been set.
+   *
+   * @return array
+   *   A list of message arrays.
+   */
+  public function getMessages() {
+    return $this->messages;
+  }
+
+}
